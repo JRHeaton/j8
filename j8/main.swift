990394501be95fe7e01b38fddc561b0a3be521b3
John Heaton <jheaton.dev@gmail.com>
2015-02-19 04:15:49 -0500

    Initial Commit

---
+++ j8/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  j8
+//
+//  Created by John Heaton on 2/19/15.
+//  Copyright (c) 2015 John Heaton. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
